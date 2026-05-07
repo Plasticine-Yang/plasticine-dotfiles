4d3b79d3afdbae9d6d152adec480fe631de33318
yangwenfeng.0719 <yangwenfeng.0719@bytedance.com>
2026-05-08 00:46:04 +0800

    fix(zsh配置): 统一使用$HOME变量替代~符号
    修复antidote安装脚本中路径引用不一致的问题，使用$HOME变量替代~符号以提高可读性和一致性

--- zsh-config/antidote/install.zsh
+++ zsh-config/antidote/install.zsh
@@ -1,12 +1,12 @@
 # clone antidote if not installed
-if [[ ! -d "${ZDOTDIR:-~}/.antidote" ]]; then
+if [[ ! -d "${ZDOTDIR:-$HOME}/.antidote" ]]; then
   echo "clonling antidote repository..."
-  git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+  git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
 else
   echo "antidote repository already cloned, skip clone."
 fi
 
-source ${ZDOTDIR:-~}/.antidote/antidote.zsh
+source ${ZDOTDIR:-$HOME}/.antidote/antidote.zsh
 
 # link .zshrc
 cp ~/.plasticine-dotfiles/zsh-config/antidote/.zshrc ~/.zshrc
@@ -16,8 +16,5 @@ cp ~/.plasticine-dotfiles/zsh-config/antidote/.zsh_plugins.txt ~/.zsh_plugins.tx
 
 # link .p10k.zsh
 cp ~/.plasticine-dotfiles/zsh-config/antidote/.p10k.zsh ~/.p10k.zsh
-
-# install fnm
-curl -fsSL https://fnm.vercel.app/install | bash
 
 source ~/.zshrc
